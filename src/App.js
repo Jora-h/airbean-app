df629142da02a62df73f7f2fde44f1ec7e1aabb4
Hajar Hamza <hajar.hamza1@gmail.com>
2022-03-24 17:50:18 +0100

    edited code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Home from './views/Home';
+import Navigation from './views/Navigation';
+import About from './views/About';
+import Menu from './views/Menu';
+import Cart from './views/Cart';
+import Status from './views/Status';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+  <Routes>
+    <Route path="/Home" element={ <Home /> } />
+    <Route path="/Navigation" element={ <Navigation /> } />
+    <Route path="/About" element={ <About /> } />
+    <Route path="/Menu" element={ <Menu /> } />
+    <Route path="/Cart" element={ <Cart /> } />
+    <Route path="/Status" element={ <Status/> } />
+
+  </Routes>
+  </BrowserRouter>
     </div>
   );
 }
--- src/views/Menu.js
+++ src/views/Menu.js
@@ -1,3 +1,5 @@
 export default function Menu () {
-    
+    return(
+        <div></div>
+      )  
 }
